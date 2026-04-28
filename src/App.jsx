b63caf11c1b989acd8ab2684b2116b812c81b217
Sweetsan <coderintheworldhack100@gmail.com>
2026-04-28 21:05:28 +0530

    react-route-project created

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <About />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/PageError.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PageError() {
+  return (
+    <>
+      <div>Page 404 is Not Found</div>
+    </>
+  );
+}
+
+export default PageError;
